b73c9d8ac223449adfa5150535686f681c105f5d
I'm so amateur man <selaruang@gmail.com>
2023-08-15 11:31:02 +0700

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-image_scale = 1000
+image_scale = 1000 #1000 mean lenght of height image
 round_pose = 1 #----> less than number of image
 scale = 1 #default is 1 ---->round 2:4k | 4/3:2k | 1:FHD **is depends on your webcam res
 cam_type = "Hor" #"Hor = Horizontal cam, Ver = Vertical cam"
